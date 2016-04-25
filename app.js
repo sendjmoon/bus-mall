ab47c7607377c8331195ba1bcaa51e80cafb7dea
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-25 13:17:24 -0700

    added append-table element

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ var imageArray = ['index 0', 'index 1', 'index 2', 'index 3', 'index 4', 'index 
 
 //function to select three random images from img dir
 function randomImage() {
+  var appendTable = document.getElementById('append-table');
   var imageId = Math.floor(Math.random() * (imageArray.length));
-  // var imageTd = document.createElementById('td');
-  // imageTd.textContent = imageArray[imageId];
-  var imageTd = imageArray[imageId];
+  var imageTd = document.createElement('td');
+  imageTd.textContent = imageArray[imageId];
+  appendTable.appendChild(imageTd);
 }
