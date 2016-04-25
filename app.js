38301a0841821a322c41ee4af10838e8354f815e
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-25 12:40:45 -0700

    oops

---
+++ app.js
@@ -0,0 +1,7 @@
+//constructor function for image object
+function Image(imageName, filePath, countView, countClick) {
+  this.imageName = imageName;
+  this.filePath = filePath;
+  this.countView = countView;
+  this.countClick = countClick;
+}
