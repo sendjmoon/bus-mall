ac6ab67c3a45c3e620646d0c9b60ec273577e9e3
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-25 13:11:39 -0700

    started on rng

--- app.js
+++ app.js
@@ -1,7 +1,9 @@
-//constructor function for image object
-function Image(imageName, filePath, countView, countClick) {
-  this.imageName = imageName;
-  this.filePath = filePath;
-  this.countView = countView;
-  this.countClick = countClick;
+var imageArray = ['index 0', 'index 1', 'index 2', 'index 3', 'index 4', 'index 5'];
+
+//function to select three random images from img dir
+function randomImage() {
+  var imageId = Math.floor(Math.random() * (imageArray.length));
+  // var imageTd = document.createElementById('td');
+  // imageTd.textContent = imageArray[imageId];
+  var imageTd = imageArray[imageId];
 }
