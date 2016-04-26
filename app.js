c6b7059976d0a9543a55dcd3d9e889f48ef45680
JAMES MOON <SENDJMOON@GMAIL.COM>
2016-04-26 10:38:07 -0700

    finished click events and counters

--- app.js
+++ app.js
@@ -1,6 +1,31 @@
 var imageArray = [];
 var generatedArray = [];
 var randomIndex = 0;
+var totalClicks = 0;
+var imageBox = document.getElementById('image-box');
+
+//object constructor
+function Image(imageName, imagePath, countShown, countClicked) {
+  this.imageName = imageName;
+  this.imagePath = imagePath;
+  this.countShown = countShown;
+  this.countClicked = countClicked;
+}
+
+// generates random index number to use
+function getRandomIndex() {
+  randomIndex = Math.floor(Math.random() * imageArray.length);
+}
+
+//creates image element in HTML
+function createImage() {
+  generatedArray.push(imageArray[randomIndex].imageName);
+  var createImg = document.createElement('img');
+  createImg.src = imageArray[randomIndex].imagePath;
+  createImg.id = imageArray[randomIndex].imageName;
+  imageBox.appendChild(createImg);
+  imageArray[randomIndex].countShown++;
+}
 
 // function to select three random images from imageArray
 function generateImages() {
@@ -13,40 +38,30 @@ function generateImages() {
         if (imageArray[randomIndex].imageName != generatedArray[0] && imageArray[randomIndex].imageName != generatedArray[1] && imageArray[randomIndex].imageName != generatedArray[2]) {
           createImage();
           imageCount++;
-          console.log('imagecount: ' + imageCount);
         }
       }
     }
   }
 }
-// generates random index number to use
-function getRandomIndex() {
-  randomIndex = Math.floor(Math.random() * imageArray.length);
-}
 
-//creates image element in HTML
-function createImage() {
-  var imageBox = document.getElementById('image-box');
-  generatedArray.push(imageArray[randomIndex].imageName);
-  var createImg = document.createElement('img');
-  createImg.src = imageArray[randomIndex].imagePath;
-  createImg.id = imageArray[randomIndex].imageName;
-  imageBox.appendChild(createImg);
-}
-
+//function to handle clicking on images
 function handleImageClick(event) {
-  event.preventDefault();
+  console.log('you clicked ' + event.target.id);
+  if (totalClicks < 5) {
+    for (var i = 0; i < imageArray.length; i++) {
+      if (event.target.id === imageArray[i].imageName) {
+        imageArray[i].countClicked++;
+      }
+    }
+    imageBox.innerHTML = '';
+    generatedArray = [];
+    generateImages();
+    totalClicks++;
+    console.log(totalClicks);
+  }
 }
 
-//object constructor
-function Image(imageName, imagePath, countShown, countClicked) {
-  this.imageName = imageName;
-  this.imagePath = imagePath;
-  this.countShown = countShown;
-  this.countClicked = countClicked;
-}
-
-// target.addEventListener('click', handleImageClick);
+imageBox.addEventListener('click', handleImageClick);
 
 imageArray.push(new Image('bag', 'img/bag.jpg', 0, 0));
 imageArray.push(new Image('banana', 'img/banana.jpg', 0, 0));
